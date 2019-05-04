86afc231c38806165b87ef0691a671118a3cf6ab
idiego209 <idiego209@gmail.com>
2019-05-03 21:08:17 -0700

    Add fibonacci generator program.

---
+++ fibanicci_generator/fibanicci_generator.py
@@ -0,0 +1,18 @@
+# Fibinacci sequence generator.
+# Author: https://github.com/idiego209
+
+def fibGenerator(seq_length):
+  a, b = 0, 1
+
+  for _ in xrange(seq_length):
+    yield a
+    a, b = b, a + b
+
+def main():
+  fib_sequence = fibGenerator(10)
+
+  for num in fib_sequence:
+    print(num)
+
+if __name__ == '__main__':
+  main()
